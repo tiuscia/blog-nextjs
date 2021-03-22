9f635cfc23c2b13db36f30e405b240bee51e681e
tiuscia <tiuscia@gmail.com>
2021-03-22 14:00:16 +0100

    removed default files

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title> Create Next App </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Blog Next.js and Tailwind </h1>
+      </main>
+    </div>
+  );
+}
